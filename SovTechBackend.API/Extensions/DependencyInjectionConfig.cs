efe9d1cea59b0db8dcf2785fdbb0e14ce2834127
Yisacc Aberham <yaberham@blackboxintelligence.com>
2022-08-10 10:29:28 +0300

    first commit

---
+++ SovTechBackend.API/Extensions/DependencyInjectionConfig.cs
@@ -0,0 +1,18 @@
+﻿//using SovTechBackend.Service.categories;
+//using SovTechBackend.Service.People;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SovTechBackend.API.Extensions
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void AddScope(IServiceCollection services)
+        {
+            //Services
+            //services.AddScoped<ICategoryService, CategoryService>();
+            //services.AddScoped<IPeopleService, PeopleService>();
+            //services.AddScoped<IJokeService, JokeService>();
+
+        }
+    }
+}
